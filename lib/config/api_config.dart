b0ea25ed40b7d058e1eae12bba272bfb871217f6
Tran Nghia <tdn051105@gmail.com>
2026-01-04 22:17:46 +0700

    Cấu hình xong đường dẫn kết nối api

---
+++ lib/config/api_config.dart
@@ -0,0 +1,7 @@
+class ApiConfig {
+  static const String baseUrl = "http://10.0.2.2:5001/api";
+
+  //Các đường dẫn con
+  static const String register = "$baseUrl/auth/register";
+  static const String login = "$baseUrl/auth/login";
+}
